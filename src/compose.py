23436aee3e50d286a4f432554ab25cced10f8524
johanbrun <johannicbrun@gmail.com>
2021-09-13 16:35:07 +0200

    Shit dont work but oh well

--- src/compose.py
+++ src/compose.py
@@ -1,39 +1,62 @@
 from music21 import note, duration, key, meter, stream
-import noise_util
+import util
 
 
 class Measure:
-    notes: list[note.Note]
-    durations: list[duration.Duration]
+    durations: list[duration.Duration] = []
+    notesInMeasure: int = 4
+    ks: key.KeySignature = key.KeySignature(0)
 
     def __init__(self) -> None:
-        noise_util.gen_normal()
+        self.notes: note.Note = []
+        midiValues = util.gen_normal(60, 1, self.notesInMeasure)
+        for mv in midiValues:
+            self.notes.append(util.note_from_noise(mv, self.ks))
+            self.durations.append(duration.Duration(1))
+
+    def getStream(self):
+        s = stream.Stream()
+        for n in self.notes:
+            s.append(n)
+        return s
 
 
 class Movement:
     chordProgression: list[int]
-    measures: list[Measure]
+    numMeasures: int = 5
 
-    def __init__(self, numMeasures) -> None:
-        for i in range(numMeasures):
+    def __init__(self) -> None:
+        self.measures: list[Measure] = []
+        for i in range(self.numMeasures):
             newMeasure = Measure()
             self.measures.append(newMeasure)
+            print(newMeasure.notes)
+
+    def getStream(self):
+        s = stream.Stream()
+        for measure in self.measures:
+            s.append(measure.getStream())
+        s.show()
+        return s
 
 
 class Composition:
     k: key.Key = key.Key('C')
     ts: meter.TimeSignature = meter.TimeSignature('4/4')
     tempo: int = 120
-    numMeasures: int = 15
-    movements: list[Movement]
 
     def __init__(self) -> None:
         firstMovement = Movement()
         secondMovement = Movement()
         self.movements = [firstMovement, secondMovement, firstMovement]
 
-    def compose(self) -> None:
+    def compose(self) -> stream.Stream:
         s = stream.Stream()
         for movement in self.movements:
-            s.append(movement)
-        s.show()
+            s.append(movement.getStream())
+        return s
+
+
+c = Composition()
+s = c.compose()
+s.show()
---
+++ src/util.py
@@ -0,0 +1,30 @@
+import numpy as np
+from numpy.random import default_rng
+from music21 import key, note, pitch
+
+rng = default_rng()
+
+
+def gen_normal(mu: float, sigma: float, length: int) -> np.array:
+    '''
+    mu:     defines the average value for the noise
+    sigma:  defines variance in noise
+    length: defines amount of descrete noise values should be generated
+    '''
+    inMidiRange = mu > 0 and mu < 127 and length > 0
+    if (inMidiRange):
+        noise = rng.normal(mu, sigma, length)
+        noise = noise
+        noise = np.rint(noise)
+        return noise
+    else:
+        print('Mu not in range!')
+        return np.array([])
+
+
+def note_from_noise(p: int, ks: key.KeySignature):
+    n = note.Note(pitch.Pitch(p))
+    nStep = n.pitch.step
+    rightAccidental = ks.accidentalByStep(nStep)
+    n.pitch.accidental = rightAccidental
+    return n
