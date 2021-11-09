18e8d016314f1a16788bca90638a544ce006049d
JohanBrun <johannicbrun@gmail.com>
2021-11-09 10:19:44 +0100

    Clean up main

--- src/main.py
+++ src/main.py
@@ -1,31 +1,25 @@
 import random
 from music21 import key, meter, stream
-import compose, chordmap, melody
+import chordmap, melody
 
 def main():
     keySignature = key.KeySignature(random.randint(-6, 6))
     timeSignature = meter.TimeSignature('4/4')
     
+    # Generating chords
     chordMap = chordmap.ChordMap(keySignature, timeSignature)
     progression = chordMap.generateRepeatableProgression()
     progression += progression
     progression += progression
-    chords = chordMap.generateChords(progression)
-    numMeasures = len(chords.getElementsByClass(stream.Measure))
+    generatedChords = chordMap.generateChords(progression)
+    numMeasures = len(generatedChords.getElementsByClass(stream.Measure))
 
-    compose.Measure.ks = keySignature
-    composition = compose.Composition(numMeasures, keySignature, timeSignature)
-
+    # generating melody
     mg = melody.MelodyGraph(numMeasures, keySignature, timeSignature)
-    
-    s = stream.Score()
-    s.keySignature = keySignature
-    s.timeSignature = timeSignature
+    generatedMelody = mg.markovMelody()
 
 
-    # s.append(composition.compose())
-    s.append(mg.walkTest())
-    s.append(chords)
+    s = stream.Score([generatedMelody, generatedChords])
     s.write('midi', '../composition.midi')
     s.show()
     
