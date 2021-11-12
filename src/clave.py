6171790fe9943074d34f78e579392541bbc3680e
JohanBrun <johannicbrun@gmail.com>
2021-11-12 10:43:06 +0100

    Expand clave to generate multiple measures

--- src/clave.py
+++ src/clave.py
@@ -1,40 +1,35 @@
 from music21 import pitch, stream, note, instrument, duration, midi, tempo
 
 class ClaveGenerator:
+    def __init__(self, numMeasures: int) -> None:
+        self.numMeasures = numMeasures
+
     def generate23Clave(self):
         s = stream.Stream()
         s.insert(instrument.Woodblock())
-        mm = tempo.MetronomeMark(number=240)
+        # mm = tempo.MetronomeMark(number=240)
         # s.append(mm)
 
+        for i in range(self.numMeasures):
+            quarterClave1 = note.Note(77, quarterLength=0.5)
+            quarterClave2 = note.Note(77, quarterLength=0.5)
+            quarterClave3 = note.Note(77, quarterLength=0.5)
+            quarterClave4 = note.Note(77, quarterLength=0.5)
+            eighthClave = note.Note(77, quarterLength=0.25)
 
-        quarterClave1 = note.Note(77, quarterLength=1)
-        quarterClave2 = note.Note(77, quarterLength=1)
-        quarterClave3 = note.Note(77, quarterLength=1)
-        quarterClave4 = note.Note(77, quarterLength=1)
-        eighthClave = note.Note(77, quarterLength=0.5)
+            quarterRest1 = note.Rest(quarterLength=0.5)
+            quarterRest2 = note.Rest(quarterLength=0.5)
+            quarterRest3 = note.Rest(quarterLength=0.5)
+            eighthRest = note.Rest(quarterLength=0.25)
 
-        quarterRest1 = note.Rest(quarterLength=1)
-        quarterRest2 = note.Rest(quarterLength=1)
-        quarterRest3 = note.Rest(quarterLength=1)
-        eighthRest = note.Rest(quarterLength=0.5)
+            s.append(quarterClave1)
+            s.append(eighthRest)
+            s.append(eighthClave)
+            s.append(quarterRest1)
+            s.append(quarterClave2)
+            s.append(quarterRest2)
+            s.append(quarterClave3)
+            s.append(quarterClave4)
+            s.append(quarterRest3)
 
-        m = stream.Measure()
-        m.append(mm)
-
-        m.append(quarterClave1)
-        m.append(eighthRest)
-        m.append(eighthClave)
-        m.append(quarterRest1)
-        m.append(quarterClave2)
-        m.append(quarterRest2)
-        m.append(quarterClave3)
-        m.append(quarterClave4)
-        m.append(quarterRest3)
-
-        s.append(m)
-
-        s.show()
-
-cg = ClaveGenerator()
-cg.generate23Clave()
+        return s
